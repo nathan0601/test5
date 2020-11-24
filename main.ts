59ea3192ae05b59a4c47066b4122afe9ae96e18d
nathan0601 <71689307+nathan0601@users.noreply.github.com>
2020-11-24 15:51:33 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,54 @@
+input.onGesture(Gesture.LogoUp, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        # # # # #
+        # # # # #
+        `)
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    basic.showLeds(`
+        # # . . .
+        # # . . .
+        # # . . .
+        # # . . .
+        # # . . .
+        `)
+})
+input.onGesture(Gesture.ScreenDown, function () {
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+})
+input.onGesture(Gesture.ScreenUp, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+})
+input.onGesture(Gesture.LogoDown, function () {
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+})
+input.onGesture(Gesture.TiltRight, function () {
+    basic.showLeds(`
+        . . . # #
+        . . . # #
+        . . . # #
+        . . . # #
+        . . . # #
+        `)
+})
